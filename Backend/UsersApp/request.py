69a6cbba5fe062153f8a196b5dae11d2bcbafc6f
FronGeorge08 <fron.george09@gmail.com>
2026-03-04 23:35:47 +0200

    changes
    bla bla

--- Backend/UsersApp/request.py
+++ Backend/UsersApp/request.py
@@ -4,7 +4,7 @@ class CreateUserRequest(serializers.Serializer):
     name=serializers.CharField(max_length=20)
     username=serializers.CharField(max_length=20)
     email=serializers.EmailField()
-    password=serializers.CharField(max_length=30)
+    password=serializers.CharField(max_length=128)
     description=serializers.CharField(max_length=200)
     image=serializers.CharField(max_length=1000)
     is_active=serializers.BooleanField()
@@ -14,3 +14,7 @@ class UpdateUserRequest(serializers.Serializer):
     username = serializers.CharField(max_length=20, required=False)
     description = serializers.CharField(max_length=200, required=False)
     image = serializers.CharField(max_length=1000, required=False)
+
+class LoginRequest(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=128)
